1a0879bd70ab649f9fa12754e68f2aed65e85554
Shane <shfitzy@gmail.com>
2022-08-04 23:07:41 -0500

    Section 6 and 7 - Course Goal App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,57 @@
-import Expenses from "./components/expenses/Expenses";
-import NewExpense from "./components/new_expense/NewExpense";
-import { useState } from "react";
+import React, { useState } from 'react';
+
+import CourseGoalList from './components/CourseGoals/CourseGoalList/CourseGoalList';
+import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
+import './App.css';
 
 const App = () => {
-    const [expenses, setExpenses] = useState([]);
+  const [courseGoals, setCourseGoals] = useState([
+    { text: 'Do all exercises!', id: 'g1' },
+    { text: 'Finish the course!', id: 'g2' }
+  ]);
 
-    const addExpense = (expenseData) => {
-        setExpenses((prevState) => [...prevState, expenseData]);
-    };
+  const addGoalHandler = enteredText => {
+    setCourseGoals(prevGoals => {
+      const updatedGoals = [...prevGoals];
+      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
+      return updatedGoals;
+    });
+  };
 
-    return (
-        <div>
-            <NewExpense onAddExpense={addExpense} />
-            <Expenses items={expenses} />
-        </div>
+  const deleteItemHandler = goalId => {
+    setCourseGoals(prevGoals => {
+      const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
+      return updatedGoals;
+    });
+  };
+
+  let content = (
+    <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
+  );
+
+  if (courseGoals.length > 0) {
+    content = (
+      <CourseGoalList items={courseGoals} onDeleteItem={deleteItemHandler} />
     );
+  }
+
+  return (
+    <div>
+      <section id="goal-form">
+        <CourseInput onAddGoal={addGoalHandler} />
+      </section>
+      <section id="goals">
+        {content}
+        {/* {courseGoals.length > 0 && (
+          <CourseGoalList
+            items={courseGoals}
+            onDeleteItem={deleteItemHandler}
+          />
+        ) // <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
+        } */}
+      </section>
+    </div>
+  );
 };
 
 export default App;
